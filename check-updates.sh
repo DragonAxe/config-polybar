84c2fac3c3f74630d071803a6a183871b8e3ec3b
DragonAxe <DragonAxe@users.noreply.github.com>
2019-11-15 22:49:32 -0700

    Added arch linux pacman updates module

---
+++ check-updates.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Source: https://github.com/polybar/polybar-scripts/tree/master/polybar-scripts/updates-arch-combined
+
+if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
+    updates=0
+fi
+
+if [ "$updates" -gt 0 ]; then
+    echo "  $updates"
+else
+    echo ""
+fi
