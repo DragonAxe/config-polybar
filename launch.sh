7a37eaf3e97ab10c25e574c2fbed1c0916965b17
DragonAxe <DragonAxe@users.noreply.github.com>
2020-02-15 18:34:28 -0700

    Fixed bug where system tray won't always start on the same monitor

--- launch.sh
+++ launch.sh
@@ -9,10 +9,15 @@ while pgrep -x polybar >/dev/null; do sleep 1; done
 # Launch bar for each
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    env SHELL=/bin/sh MONITOR=$m polybar barbottom &
+    primary_m="$(xrandr --query | grep "^${m} " | grep " primary")"
+    # If primary monitor, attach system tray to polybar in primary monitor
+    if ! [[ -z "${primary_m}" ]]; then
+      env SHELL=/bin/sh MONITOR="${m}" TRAY_POS=right polybar barbottom &
+    else
+      env SHELL=/bin/sh MONITOR="${m}" TRAY_POS=none polybar barbottom &
+    fi
   done
   echo "Bars launched..."
 else
   echo "Error: Not using xrandr"
 fi
-
